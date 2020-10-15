e4f959ccf96d29fc1b3d12650e873d640fb651a7
carlosuyar <carlosarmstrong@gmail.com>
2020-10-15 13:30:41 +0000

    testeando el diccionario con un juego

--- quiz.py
+++ quiz.py
@@ -1,6 +1,13 @@
 #importo random para usarlo mas adelante
 import random
 
+def get_def_and_pop(word_list,word_dict):
+    random_index = random.randrange(len(word_list))
+    word = word_list.pop(random_index)
+    definition = word_dict.get(word)
+    return word, definition
+
+
 #una funcion para separar la palabra y la definicion
 #aca la magia para pasar el split a dos variables
 #indicando separado por , y que sea tomada solo la primer una vez.
@@ -32,6 +39,32 @@ fh.writelines(wd_set)
 word_dict = dict() #instancia un diccionario.
 for rawstring in wd_set:
     word,definition = get_word_and_definition(rawstring)
-    word_dict[word]=definition
+    word_dict[word]= definition
 
 
+wd_list = list(word_dict)
+
+for rawstring in wd_set:
+    word,definition = get_word_and_definition(rawstring)
+    word_dict[word] = definition
+
+while True:
+    wd_list = list(word_dict)
+    choice_list = []
+    
+    for x in range(4):
+        word,definition =  get_def_and_pop(wd_list, word_dict)
+        choice_list.append(definition)
+    random.shuffle(choice_list)
+    print(word)
+    print("---------------------")
+   
+    for idx, choice in enumerate(choice_list):
+        print(idx+1,choice)
+    choice = int(input('Enter 1, 2, 3 or 4, 0 to exit'))
+    if choice_list[choice - 1] == definition:
+        print("Correct! \n")
+    elif choice == 0:
+        exit(0)
+    else:
+        print("Incorrect")
